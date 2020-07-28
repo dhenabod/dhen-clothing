22e548378c6b67625048585858f56b54b11a2d69
Dhen Abod <dhenabod@gmail.com>
2020-07-28 17:54:12 +0800

    implemented firebase utils, included ability to store authenticated users into the firestore database

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -13,6 +13,32 @@ const config = {
     measurementId: "G-1NCPC7714E",
 };
 
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShot = await userRef.get();
+    // check if data already exist
+    if (!snapShot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        // will try to insert a user, using .set()
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData,
+            });
+        } catch (error) {
+            console.log("Error creating user: ", error.message);
+        }
+    }
+    return userRef;
+};
+
 firebase.initializeApp(config);
 
 export const auth = firebase.auth();
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,12 +1,14 @@
 import React from "react";
-import SingIn from "../../components/sign-in/sign-in.component";
+import SignIn from "../../components/sign-in/sign-in.component";
+import SignUp from "../../components/sign-up/sign-up.component";
 
 import "./sign-in-and-sign-up.styles.scss";
 
 const SignInAndSignUp = () => {
     return (
         <div className="sign-in-and-sign-up">
-            <SingIn></SingIn>
+            <SignIn></SignIn>
+            <SignUp></SignUp>
         </div>
     );
 };
