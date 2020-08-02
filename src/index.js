e5e75406b2eb2746a80aedfead1a0dbc64e1a0fa
Dhen Abod <dhenabod@gmail.com>
2020-08-02 17:02:25 +0800

    added redux persist

--- src/index.js
+++ src/index.js
@@ -2,19 +2,23 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 
+import { PersistGate } from "redux-persist/integration/react";
+
 import { Provider } from "react-redux";
 // The <Provider /> makes the Redux store available to any nested components that have been wrapped in the connect() function.
 
 import "./index.css";
 import App from "./App";
 
-import store from "./redux/store";
+import { store, persistor } from "./redux/store";
 
 ReactDOM.render(
     <React.StrictMode>
         <Provider store={store}>
             <BrowserRouter>
-                <App />
+                <PersistGate persistor={persistor}>
+                    <App />
+                </PersistGate>
             </BrowserRouter>
         </Provider>
     </React.StrictMode>,
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,16 @@
 import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 
+import { persistStore } from "redux-persist";
+
 import rootReducer from "./root-reducer";
 
 // the middleware that the store is expecting from redux is going to be an array
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
-export default store;
+// create persisted version of the store
+export const persistor = persistStore(store);
+
+export default { store, persistor };
